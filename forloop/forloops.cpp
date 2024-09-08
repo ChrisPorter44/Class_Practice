66d8d8f0d07038013f13f9e776e3237028c0f5b7
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-09-08 00:38:06 +0000

    a

--- forloop/forloops.cpp
+++ forloop/forloops.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-int main(){
-    
+int main() {
+    int number;
+    cin >> number;
 
-    for(int i=0;i>=10;i++){
-        cout<<"i is "<<i<<endl;
-
+    for (int i = 0; i <= 10; i++) {
+        cout << "i is " << i << endl;
+        cin >> number;
     }
 
     return 0;
-    
 }
