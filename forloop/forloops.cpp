23188f52ae0ae2949b428cf2ea4f3e5a9efb67b1
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-09-08 00:31:39 +0000

    loop

---
+++ forloop/forloops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include <string>
+
+using namespace std;
+
+int main(){
+    
+
+    for(int i=0;i>=10;i++){
+        cout<<"i is "<<i<<endl;
+
+    }
+
+    return 0;
+    
+}
