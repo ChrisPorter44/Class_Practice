8408d932aff3ffe2e9e7bfacadd2cfac89855eb0
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-09-08 08:46:42 +0000

    forchange

---
+++ forloop/for1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+   
+
+int main() {
+
+    int num;
+    
+    cout << "Enter a positive integer: "<<endl;
+    
+
+        for (int i = 1; i<= 10; i++) {
+        cin>>num;
+
+        cout<< num<< endl;
+        
+    }
+    
+    return 0;
+}
