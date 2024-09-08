3293aec02d65b2b191bd94c76dd2149c99f8cd8b
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-09-08 07:54:04 +0000

    a

--- forloop/forloops.cpp
+++ forloop/forloops.cpp
@@ -2,12 +2,21 @@
 #include <string>
 
 using namespace std;
-
-
+   
 
 int main() {
-        for (int i = 10; i>= 0; i-=1) {
-        cout << i << " "<<endl;
+
+    int sum;
+    int num;
+    sum = 0;
+
+    cout << "Enter a positive integer: ";
+    cin >> num;
+
+        for (int i = 1; i<= num; i++) {
+        sum += i;
+        
     }
+    cout << "Sum = " << sum << endl;
     return 0;
 }
