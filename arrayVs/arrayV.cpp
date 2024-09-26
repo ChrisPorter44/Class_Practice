a8d5b55ed06f985bc87f8a65692791db9bb0725b
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-09-26 15:29:29 +0000

    arrayvs

---
+++ arrayVs/arrayV.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <array>
+
+int main(){
+    int array[3]={1,2,3};
+    std::cout<<"Array";
+    for (int i = 0;i<3,++i)
+        std::cout<<array[i]<<" ";
+    std::cout<<std::endl;
+
+    //Vector
+    std::vector<int>vec={1,2,3};
+    vec.pushback(4);//adding element
+    std::cout<<"Vector: ";
+    for(int i=0;i<vec.size();++i)
+        std::cout<<vec[i]<<" ";
+    std::cout<<std::endl;
+
+    return 0;
+
+}
