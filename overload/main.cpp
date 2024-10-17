8e1f6311da0daf8f424b1ca624d41553c9949639
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-10-17 16:22:04 +0000

    overld

--- overload/main.cpp
+++ overload/main.cpp
@@ -9,17 +9,17 @@ private:
 
 public:
     Student(const std::string& name,int studnetID,const std::string& address)
-    :name(name),stdudentID(studentID),address (address){}
+    :name(name),studentID(studentID),address (address){}
     friend std::ostream& operator<<(std::ostream& os,const Student& student){
         os<<"Name: "<<student.name<<'\n';
         os<<"Student ID"<<student.studentID<<'\n';
         os<<"Address"<<student.address<<'\n';
-        return os
+        return os;
     }
 };
 int main(){
 
     Student student("John Doe",12345,"123 USA");
     std::cout<<student;
-    return 0
+    return 0;
 }
