2baa7877313d7b246121524e2937be890b189c25
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-10-29 15:47:46 +0000

    file

---
+++ FileAO/file.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main{
+    std::ifstream  file("example file");
+    std::string line;
+
+    if(lfile){
+        std::cerr<<"File didnt open"
+        std::cerr<<"file didnt open where is it"<<std::endl;}
+        return 1
+    while(std::getline(file,line){
+            std::cout<<""<<line<<""<<std::endl;
+
+        }
+
+    
+    file.close();
+    return 0
+}
