3e7fb3e391db53d67ea6d2fe86149c51654182f3
ChrisPorter44 <156853203+ChrisPorter44@users.noreply.github.com>
2024-11-19 17:41:24 +0000

    inputint

--- inputNormal/inputInt.cpp
+++ inputNormal/inputInt.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
-#include <interators>
+#include <iterators>
 
 int main(){
     std::vector<int> numbers;
     std::cout<<"Enter Integers (Ctrl+D to End): ";
 
     std::istream_iterator<int> start(std::cin),end;
-
+    numbers.assign(start,end);
+    
     std::cout<<"You entered: ";
     for(int num : numbers){
         std::cout<<num<<" ";
